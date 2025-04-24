39c80117490523410100cd2864a05870b1b5b60f
riaz37 <riaz37.ipe@gmail.com>
2025-04-25 00:55:26 +0600

    fixed duplicate header

--- src/app/(user)/dashboard/layout.tsx
+++ src/app/(user)/dashboard/layout.tsx
@@ -1,4 +1,3 @@
-import { Header } from "@/components/layout/header";
 import { auth } from "@/server/auth";
 import { redirect } from "next/navigation";
 import { ReactNode } from "react";
@@ -16,12 +15,9 @@ export default async function DashboardLayout({
   }
 
   return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <div className="flex-1">
-        <div className="container max-w-6xl mx-auto py-10 px-4 md:px-8">
-          {children}
-        </div>
+    <div className="flex-1">
+      <div className="container max-w-6xl mx-auto py-10 px-4 md:px-8">
+        {children}
       </div>
     </div>
   );
